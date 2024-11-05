38eda24eba02cf7df09c3957bc7f81aa1f8bc73e
user <shareefm@umd.edu>
2024-11-05 04:16:53 -0500

    - Allow the student to attempt quiz, and he should see the result graded properly in all cases. - Fix several logic bugs.

---
+++ src/main/java/com/github/ilim/backend/exception/exceptions/AdminCantAttemptQuizzesException.java
@@ -0,0 +1,10 @@
+package com.github.ilim.backend.exception.exceptions;
+
+import java.util.UUID;
+
+public class AdminCantAttemptQuizzesException extends Throwable {
+    public AdminCantAttemptQuizzesException(String userId, UUID quizId) {
+        super("User(%s) is admin and admins cannot attempt a quiz, including Quizz(%s)"
+            .formatted(userId, quizId));
+    }
+}
